82e6d76cc21257d54856806b23429a9e024af637
William Jones <wjonesusna2012@gmail.com>
2019-10-21 11:14:14 -0500

    Test code to connect to dockerized quorum network

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import Web3 from 'web3';
+import ganache  from 'ganache-core';
 
 class App extends Component {
+  componentWillMount() {
+    this.loadBlockchainData();
+  }
+
+  async loadBlockchainData() {
+    const web3 = new Web3("http://localhost:22001");
+    const accounts = await web3.eth.getAccounts();
+    console.log(accounts, 'AC');
+    this.setState({account: accounts[0]})
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {account: ''};
+  }
   render() {
-    const App = () => (
-      <div>
-        <Switch>
-          <Route exact path='/' component={Home}/>
-          <Route path='/list' component={List}/>
-        </Switch>
-      </div>
-    )
     return (
-      <Switch>
-        <App/>
-      </Switch>
+      <div>
+        <h1>Quorum Test</h1>
+        <p>Account for this node: {this.state.account}</p>
+      </div>
     );
   }
 }
